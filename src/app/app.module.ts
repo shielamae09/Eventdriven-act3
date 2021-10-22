174ba329dcf57e438655a2decc7d97ea4a00f960
shie <89240695+shielamae09@users.noreply.github.com>
2021-10-22 19:36:30 +0800

    kakasadd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
-
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from '.app/pages/home/home.component';
+import { GalleryComponent } from '.app/pages/gallery/gallery.component';
+import { ProfileComponent } from '.app/pages/profile/profile.component';
 
 const routes: Routes =[
   {path: "home", component: HomeComponent},
   {path: "**", redirectTo: "home"},
 ]
-
 
 @NgModule({
   declarations: [
