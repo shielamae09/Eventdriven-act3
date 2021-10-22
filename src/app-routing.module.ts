ac523f4797ed7bd8f34e1b1b215a5d8e7dc2868f
shie <89240695+shielamae09@users.noreply.github.com>
2021-10-22 15:41:22 +0800

    just added

---
+++ src/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "gallery", component: GalleryComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'**',component:HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
