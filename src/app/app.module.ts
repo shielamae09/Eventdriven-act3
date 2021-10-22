77ca63a0c5ff6dc2743407a00ddfb131b6bd202c
shie <89240695+shielamae09@users.noreply.github.com>
2021-10-22 14:56:34 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+   
   ],
   imports: [
-    BrowserModule
+    
+    MatIconModule,
+    MatToolbarModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
